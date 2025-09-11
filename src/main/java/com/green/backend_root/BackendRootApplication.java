12dbba30598fa6e350d4998f7a4a2116274250e3
rush020114 <rush020114@gmail.com>
2025-09-11 15:26:54 +0900

    프로젝트 스프링 생성

---
+++ src/main/java/com/green/backend_root/BackendRootApplication.java
@@ -0,0 +1,13 @@
+package com.green.backend_root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/backend_root/BackendRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.backend_root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

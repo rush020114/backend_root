6dc475d25d38b839fa4473ee2a5ce3ab13e3edcd
rush020114 <rush020114@gmail.com>
2025-10-01 12:18:08 +0900

    로그인 가능 커밋

--- src/main/java/com/green/backend_root/growing/controller/GrowingController.java
+++ src/main/java/com/green/backend_root/growing/controller/GrowingController.java
@@ -2,8 +2,11 @@ package com.green.backend_root.growing.controller;
 
 import com.green.backend_root.growing.dto.GrowingDTO;
 import com.green.backend_root.growing.service.GrowingService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +27,17 @@ public class GrowingController {
     return growingService.getGrowingList();
   }
 
+  // 7일 간 센서 데이터 조회
+  @GetMapping("/weekly")
+  public ResponseEntity<?> getWeeklyGrowingList(){
+    try {
+      return ResponseEntity
+              .status(HttpStatus.OK)
+              .body(growingService.getWeeklyGrowingList());
+    } catch (Exception e) {
+      return ResponseEntity
+              .status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .body("조회 중 서버 오류 발생");
+    }
+  }
 }
